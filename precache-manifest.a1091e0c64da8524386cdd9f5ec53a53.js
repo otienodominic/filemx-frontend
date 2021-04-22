19e708ed5a314caeec09c89f0c81ffc4557509ca
Dominic Ngalo <otienodominic@gmail.com>
2021-04-22 09:55:23 +0300

    Updates

---
+++ precache-manifest.a1091e0c64da8524386cdd9f5ec53a53.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f08f90ce0c25e599b20d869423c7d07c",
+    "url": "/filemx-frontend/index.html"
+  },
+  {
+    "revision": "c09a5b2531a3ef2e43fe",
+    "url": "/filemx-frontend/static/css/main.6552a66d.chunk.css"
+  },
+  {
+    "revision": "6bc3c4fd7803ba0c6bc5",
+    "url": "/filemx-frontend/static/js/2.04399c78.chunk.js"
+  },
+  {
+    "revision": "cf31c5ff25652711d0e0c03fe52c7b20",
+    "url": "/filemx-frontend/static/js/2.04399c78.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c09a5b2531a3ef2e43fe",
+    "url": "/filemx-frontend/static/js/main.94dbd389.chunk.js"
+  },
+  {
+    "revision": "738389fe34d96a338050",
+    "url": "/filemx-frontend/static/js/runtime-main.f8339d3a.js"
+  },
+  {
+    "revision": "5c0c63fb62ec323a10598d7e9de4609e",
+    "url": "/filemx-frontend/static/media/memories.5c0c63fb.png"
+  }
+]);
